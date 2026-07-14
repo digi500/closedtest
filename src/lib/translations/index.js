fd1f187254a88c9624163985588ae35b86ffa426
digi500 <digitalist500@gmail.com>
2026-07-14 03:05:33 +0300

    feat: Add 114 regional languages support and optimize dropdown design with representative flags

--- src/lib/translations/index.js
+++ src/lib/translations/index.js
@@ -6,6 +6,114 @@ import { ptBR } from './ptBR';
 import { ptPT } from './ptPT';
 import { esES } from './esES';
 import { esMX } from './esMX';
+import { de } from './de';
+import { fr } from './fr';
+import { it } from './it';
+import { ru } from './ru';
+import { zh } from './zh';
+import { ja } from './ja';
+import { ko } from './ko';
+import { hi } from './hi';
+import { ar } from './ar';
+import { nl } from './nl';
+import { pl } from './pl';
+import { sv } from './sv';
+import { no } from './no';
+import { da } from './da';
+import { fi } from './fi';
+import { el } from './el';
+import { he } from './he';
+import { cs } from './cs';
+import { ro } from './ro';
+import { hu } from './hu';
+import { uk } from './uk';
+import { id } from './id';
+import { vi } from './vi';
+import { th } from './th';
+import { tl } from './tl';
+import { ms } from './ms';
+import { sk } from './sk';
+import { bg } from './bg';
+import { hr } from './hr';
+import { sr } from './sr';
+import { zhTW } from './zhTW';
+import { ca } from './ca';
+import { sl } from './sl';
+import { et } from './et';
+import { lv } from './lv';
+import { lt } from './lt';
+import { is } from './is';
+import { ga } from './ga';
+import { mt } from './mt';
+import { sq } from './sq';
+import { mk } from './mk';
+import { bs } from './bs';
+import { ka } from './ka';
+import { hy } from './hy';
+import { az } from './az';
+import { kk } from './kk';
+import { uz } from './uz';
+import { fa } from './fa';
+import { ur } from './ur';
+import { bn } from './bn';
+import { pa } from './pa';
+import { mr } from './mr';
+import { te } from './te';
+import { ta } from './ta';
+import { gu } from './gu';
+import { kn } from './kn';
+import { ml } from './ml';
+import { si } from './si';
+import { ne } from './ne';
+import { my } from './my';
+import { km } from './km';
+import { lo } from './lo';
+import { mn } from './mn';
+import { tg } from './tg';
+import { ky } from './ky';
+import { tk } from './tk';
+import { eu } from './eu';
+import { gl } from './gl';
+import { lb } from './lb';
+import { cy } from './cy';
+import { af } from './af';
+import { sw } from './sw';
+import { zu } from './zu';
+import { xh } from './xh';
+import { am } from './am';
+import { yo } from './yo';
+import { ig } from './ig';
+import { so } from './so';
+import { mg } from './mg';
+import { eo } from './eo';
+import { la } from './la';
+import { haw } from './haw';
+import { mi } from './mi';
+import { sm } from './sm';
+import { su } from './su';
+import { jv } from './jv';
+import { hmn } from './hmn';
+import { yi } from './yi';
+import { co } from './co';
+import { fy } from './fy';
+import { sa } from './sa';
+import { bo } from './bo';
+import { gn } from './gn';
+import { be } from './be';
+import { tt } from './tt';
+import { gd } from './gd';
+import { fo } from './fo';
+import { or } from './or';
+import { sd } from './sd';
+import { as } from './as';
+import { qu } from './qu';
+import { ay } from './ay';
+import { om } from './om';
+import { ti } from './ti';
+import { rw } from './rw';
+import { sn } from './sn';
+import { fj } from './fj';
+import { to } from './to';
 
 export const translations = {
   tr: tr,
@@ -18,5 +126,229 @@ export const translations = {
   'pt-PT': ptPT,
   es: esES,
   'es-ES': esES,
-  'es-MX': esMX
+  'es-MX': esMX,
+  de: de,
+  'de-DE': de,
+  fr: fr,
+  'fr-FR': fr,
+  it: it,
+  'it-IT': it,
+  ru: ru,
+  'ru-RU': ru,
+  zh: zh,
+  'zh-CN': zh,
+  'zh-TW': zhTW,
+  ja: ja,
+  'ja-JP': ja,
+  ko: ko,
+  'ko-KR': ko,
+  hi: hi,
+  'hi-IN': hi,
+  ar: ar,
+  'ar-SA': ar,
+  nl: nl,
+  'nl-NL': nl,
+  pl: pl,
+  'pl-PL': pl,
+  sv: sv,
+  'sv-SE': sv,
+  no: no,
+  'no-NO': no,
+  da: da,
+  'da-DK': da,
+  fi: fi,
+  'fi-FI': fi,
+  el: el,
+  'el-GR': el,
+  he: he,
+  'he-IL': he,
+  cs: cs,
+  'cs-CZ': cs,
+  ro: ro,
+  'ro-RO': ro,
+  hu: hu,
+  'hu-HU': hu,
+  uk: uk,
+  'uk-UA': uk,
+  id: id,
+  'id-ID': id,
+  vi: vi,
+  'vi-VN': vi,
+  th: th,
+  'th-TH': th,
+  tl: tl,
+  'tl-PH': tl,
+  ms: ms,
+  'ms-MY': ms,
+  sk: sk,
+  'sk-SK': sk,
+  bg: bg,
+  'bg-BG': bg,
+  hr: hr,
+  'hr-HR': hr,
+  sr: sr,
+  'sr-RS': sr,
+  'zh-HK': zhTW,
+  ca: ca,
+  'ca-ES': ca,
+  sl: sl,
+  'sl-SI': sl,
+  et: et,
+  'et-EE': et,
+  lv: lv,
+  'lv-LV': lv,
+  lt: lt,
+  'lt-LT': lt,
+  is: is,
+  'is-IS': is,
+  ga: ga,
+  'ga-IE': ga,
+  mt: mt,
+  'mt-MT': mt,
+  sq: sq,
+  'sq-AL': sq,
+  mk: mk,
+  'mk-MK': mk,
+  bs: bs,
+  'bs-BA': bs,
+  ka: ka,
+  'ka-GE': ka,
+  hy: hy,
+  'hy-AM': hy,
+  az: az,
+  'az-AZ': az,
+  kk: kk,
+  'kk-KZ': kk,
+  uz: uz,
+  'uz-UZ': uz,
+  fa: fa,
+  'fa-IR': fa,
+  ur: ur,
+  'ur-PK': ur,
+  bn: bn,
+  'bn-BD': bn,
+  pa: pa,
+  'pa-IN': pa,
+  'pa-PK': pa,
+  mr: mr,
+  'mr-IN': mr,
+  te: te,
+  'te-IN': te,
+  ta: ta,
+  'ta-IN': ta,
+  gu: gu,
+  'gu-IN': gu,
+  kn: kn,
+  'kn-IN': kn,
+  ml: ml,
+  'ml-IN': ml,
+  si: si,
+  'si-LK': si,
+  ne: ne,
+  'ne-NP': ne,
+  my: my,
+  'my-MM': my,
+  km: km,
+  'km-KH': km,
+  lo: lo,
+  'lo-LA': lo,
+  mn: mn,
+  'mn-MN': mn,
+  tg: tg,
+  'tg-TJ': tg,
+  ky: ky,
+  'ky-KG': ky,
+  tk: tk,
+  'tk-TM': tk,
+  eu: eu,
+  'eu-ES': eu,
+  gl: gl,
+  'gl-ES': gl,
+  lb: lb,
+  'lb-LU': lb,
+  cy: cy,
+  'cy-GB': cy,
+  af: af,
+  'af-ZA': af,
+  sw: sw,
+  'sw-KE': sw,
+  'sw-TZ': sw,
+  zu: zu,
+  'zu-ZA': zu,
+  xh: xh,
+  'xh-ZA': xh,
+  am: am,
+  'am-ET': am,
+  yo: yo,
+  'yo-NG': yo,
+  ig: ig,
+  'ig-NG': ig,
+  so: so,
+  'so-SO': so,
+  mg: mg,
+  'mg-MG': mg,
+  eo: eo,
+  'eo-UN': eo,
+  la: la,
+  'la-VA': la,
+  haw: haw,
+  'haw-US': haw,
+  mi: mi,
+  'mi-NZ': mi,
+  sm: sm,
+  'sm-WS': sm,
+  su: su,
+  'su-ID': su,
+  jv: jv,
+  'jv-ID': jv,
+  hmn: hmn,
+  'hmn-CN': hmn,
+  yi: yi,
+  'yi-IL': yi,
+  co: co,
+  'co-FR': co,
+  fy: fy,
+  'fy-NL': fy,
+  sa: sa,
+  'sa-IN': sa,
+  bo: bo,
+  'bo-CN': bo,
+  gn: gn,
+  'gn-PY': gn,
+  be: be,
+  'be-BY': be,
+  tt: tt,
+  'tt-RU': tt,
+  gd: gd,
+  'gd-GB': gd,
+  fo: fo,
+  'fo-FO': fo,
+  or: or,
+  'or-IN': or,
+  sd: sd,
+  'sd-PK': sd,
+  'sd-IN': sd,
+  as: as,
+  'as-IN': as,
+  qu: qu,
+  'qu-PE': qu,
+  'qu-BO': qu,
+  'qu-EC': qu,
+  ay: ay,
+  'ay-BO': ay,
+  'ay-PE': ay,
+  om: om,
+  'om-ET': om,
+  'om-KE': om,
+  ti: ti,
+  'ti-ER': ti,
+  'ti-ET': ti,
+  rw: rw,
+  'rw-RW': rw,
+  sn: sn,
+  'sn-ZW': sn,
+  fj: fj,
+  'fj-FJ': fj,
+  to: to,
+  'to-TO': to
 };
